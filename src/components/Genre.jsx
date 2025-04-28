e7cccfb6c98c9633bb704658f5ee85296a317826
helys-l <dwiphelyas@gmail.com>
2025-04-28 21:21:53 +0700

    Update Genre.jsx

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -2,15 +2,21 @@ import { fetchAnime } from '../utils/fetch.js';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
 export default function Genre() {
   const [genres, setGenres] = useState([]);
+  
+  // Daftar kata/genre yang ingin difilter
+  const filterWords = ['doujinshi', 'lolicon', 'yaoi','ecchi', 'shotacon','yuri',]; // Tambahkan kata-kata yang ingin difilter
 
   useEffect(() => {
     async function getGenres() {
       const data = await fetchAnime('genres');
       if (data && data.genres) {
-        setGenres(data.genres);
+        // Filter genre yang tidak ada dalam filterWords
+        const filteredGenres = data.genres.filter(genre => 
+          !filterWords.includes(genre.label.toLowerCase()) // Periksa apakah genre ada dalam filterWords
+        );
+        setGenres(filteredGenres);
       }
     }
     getGenres();
@@ -22,7 +28,7 @@ export default function Genre() {
       <div className="text-justify text-sm acc p-2 rounded-lg">
         {genres.map((genre, index) => (
           <span key={index}>
-           <Link to={`/genre/${genre.label.toLowerCase()}`} className='hover:underline'> {genre.label}</Link>
+            <Link to={`/genre/${genre.label.toLowerCase()}`} className='hover:underline'> {genre.label}</Link>
             {index !== genres.length - 1 && ', '}
           </span>
         ))}
